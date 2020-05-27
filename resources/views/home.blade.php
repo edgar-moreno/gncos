98dbee0fff3d227327da67e1eb07981f2bb8d3eb
EdgarMoreno <emoreno636@misena.edu.co>
2020-05-27 07:00:30 -0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,42 +5,12 @@
 @extends('layouts.app')
 
 @section('content')
+
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <nav class="navbar navbar-dark bg-dark">
-            </nav>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">inicio</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                  <ul class="navbar-nav">
-                    <li class="nav-item active">
-                      <a class="nav-link" href="#">usuario <span class="sr-only">(current)</span></a>
-                    </li>
-                        <li class="nav-item">
-                      <a class="nav-link" href="#">nomina</a>
-                    </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="#">noticias</a>
-                      </li>
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown link
-                      </a>
-                      <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-              </nav>
-                    
-                <div class="card">
+          <div class="card">
                 <div class="card-header text-light bg-dark">Gestor de nómina cos</div>
                  <div class="card-body">
                     @if (session('status'))
@@ -48,6 +18,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
+<<<<<<< HEAD
 
                     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                         <div class="carousel-inner">
@@ -70,6 +41,11 @@
                           <span class="sr-only">Next</span>
                         </a>
                     </div>
+=======
+                
+            
+                    <h2>Bienvenido</h2>
+>>>>>>> ramaAdriana
                     </br>
                     <h3>Bienvenido</h3>
                     </br>
