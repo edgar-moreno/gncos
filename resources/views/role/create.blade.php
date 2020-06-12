3aed9e716817cd880abdd232765d42bc4d4c65c6
EdgarMoreno <emoreno636@misena.edu.co>
2020-06-12 06:21:53 -0500

    Update create.blade.php

--- resources/views/role/create.blade.php
+++ resources/views/role/create.blade.php
@@ -13,56 +13,127 @@
                 <div class="card-header text-light bg-dark">Crear un rol</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    
+                    @include('custom.message')
 
                     <form action="{{route('role.store')}}" method="POST">
                         @csrf
                         <div class="container">
                             <form>
                                 <div class="form-group">
-                                  <label for="name">Nombre del nuevo rol</label>
-                                  <input type="text" class="form-control" id="name" name="name" aria-describedby="nameHelp" placeholder="Ej. Asistente de nómina">
-                                  <small id="nameHelp" class="form-text text-muted">Colocar el cargo al cual se le asignarán permisos</small>
+                                    <label for="name">
+                                        Nombre del nuevo rol
+                                    </label>
+                                    <input type="text" 
+                                    class="form-control" 
+                                    id="name" 
+                                    name="name" 
+                                    aria-describedby="nameHelp" 
+                                    placeholder="Ej. Asistente de nómina"
+                                    value="{{old('name')}}">
+                                    <small id="nameHelp" 
+                                    class="form-text text-muted">
+                                        Colocar el cargo al cual se le asignarán 
+                                        permisos
+                                    </small>
                                 </div>
                                 <div class="form-group">
-                                    <label for="slug">Slug</label>
-                                    <input type="text" class="form-control" id="slug" name="slug" aria-describedby="slugHelp" placeholder="Ej. secretaria, coordinador, soporte">
-                                    <small id="nameHelp" class="form-text text-muted">Colocar una palabra corta que describa el cargo</small>
+                                    <label for="slug">
+                                        Slug
+                                    </label>
+                                    <input type="text" 
+                                    class="form-control" 
+                                    id="slug" name="slug" 
+                                    aria-describedby="slugHelp" 
+                                    placeholder=
+                                    "Ej. secretaria, coordinador, soporte"
+                                    value="{{old('slug')}}">
+                                    <small id="nameHelp" 
+                                    class="form-text text-muted">
+                                        Colocar una palabra corta que describa 
+                                        el cargo
+                                    </small>
                                 </div>
                                 <div class="form-group">
-                                    <label for="description">Descripción</label>
-                                    <input type="text" class="form-control" id="description" name="description" aria-describedby="descriptionHelp" placeholder="Ej. con permisos para...">
-                                    <small id="descriptionHelp" class="form-text text-muted">Colocar los aspectos que diferencian a este de otros roles</small>
+                                    <label for="description">
+                                        Descripción
+                                    </label>
+                                    <input type="text" 
+                                    class="form-control" 
+                                    id="description" 
+                                    name="description" 
+                                    aria-describedby="descriptionHelp" 
+                                    placeholder="Ej. con permisos para..."
+                                    value="{{old('description')}}">
+                                    <small id="descriptionHelp" 
+                                    class="form-text text-muted">
+                                        Colocar los aspectos que diferencian 
+                                        a este de otros roles
+                                    </small>
                                 </div>
                                 
                                 <p>Acceso Total
                                     <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="full-access" id="full-accessyes" value="yes">
-                                        <label class="form-check-label" for="full-accessyes">Si</label>
+                                        <input class="form-check-input" 
+                                        type="radio" 
+                                        name="full-access" 
+                                        id="full-accessyes" 
+                                        value="yes"
+                                        @if (old('full-access')=="yes")
+                                            checked
+                                        @endif>
+                                        <label class="form-check-label" 
+                                        for="full-accessyes">
+                                            Si
+                                        </label>
                                     </div>
                                     <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="full-access" id="full-accessno" value="no" checked>
-                                        <label class="form-check-label" for="full-accessno">No</label>
+                                        <input class="form-check-input" 
+                                        type="radio" 
+                                        name="full-access" 
+                                        id="full-accessno" 
+                                        value="no" 
+                                        @if (old('full-access')=="no")
+                                            checked
+                                        @endif
+                                        @if (old('full-access')===null)
+                                            checked
+                                        @endif>
+                                        <label class="form-check-label" 
+                                        for="full-accessno">
+                                            No
+                                        </label>
                                     </div>
                                 </p>
                                 
                                 <p>Permisos a asignar
                                     @foreach ($permissions as $permission)
                                         <div class="form-group form-check">
-                                            <input type="checkbox" class="form-check-input" id="permission_{{$permission->id}}" value="{{$permission->id}}" name="permission[]">
-                                            <label class="form-check-label" for="permission_{{$permission->id}}">{{$permission->id}} - {{$permission->name}}</label>
+                                            <input type="checkbox" 
+                                            class="form-check-input" 
+                                            id="permission_{{$permission->id}}" 
+                                            value="{{$permission->id}}" 
+                                            name="permission[]"
+                                            @if (is_array(old('permission')) 
+                                            && in_array("$permission->id",
+                                            old('permission')))
+                                            checked    
+                                            @endif>
+                                            <label class="form-check-label" 
+                                            for="permission_{{$permission->id}}">
+                                                {{$permission->id}} 
+                                                - {{$permission->name}}
+                                            </label>
                                         </div>    
                                     @endforeach
                                 </p>
 
-                                <button type="submit" class="btn btn-outline-secondary">Guardar</button>
+                                <button type="submit" 
+                                class="btn btn-outline-secondary">
+                                    Guardar
+                                </button>
                               </form>
                         </div>
-
                     </form>
                 </div>
             </div>
