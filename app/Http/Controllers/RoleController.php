a4e90c64389150985547a33cf6a39b17f81f51b6
EdgarMoreno <emoreno636@misena.edu.co>
2020-06-17 06:54:39 -0500

    Update RoleController.php

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -69,9 +69,18 @@ class RoleController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Role $role)
     {
-        //
+        $permission_role=[];
+        
+        foreach($role->permissions as $permission) {
+            $permission_role[]=$permission->id;
+        }
+
+        $permissions = Permission::get();
+
+        return view('role.view',compact('permissions','role','permission_role'));
+
     }
 
     /**
@@ -80,9 +89,17 @@ class RoleController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Role $role)
     {
-        //
+        $permission_role=[];
+        
+        foreach($role->permissions as $permission) {
+            $permission_role[]=$permission->id;
+        }
+
+        $permissions = Permission::get();
+
+        return view('role.edit',compact('permissions','role','permission_role'));
     }
 
     /**
@@ -92,9 +109,29 @@ class RoleController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Role $role)
     {
-        //
+        $request->validate([
+            'name'          =>'required|max:50|unique:roles,name,' .$role->id,
+            'slug'          =>'required|max:50|unique:roles,slug,' .$role->id,
+            'full-access'   =>'required|in:yes,no'
+        ]);
+
+        $role->update($request->all());
+
+        if ($request->get('permission')) {
+            
+            //return $request->all();
+
+            $role->permissions()->sync($request->get('permission'));
+        }
+        
+        /*else{
+            return 'No existe';
+        }*/
+
+        return redirect()->route('role.index')
+        ->with('status_success','Role updated successfully');
     }
 
     /**
@@ -103,8 +140,12 @@ class RoleController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Role $role)
     {
-        //
+        $role->delete();
+        
+        return redirect()->route('role.index')
+        ->with('status_success','Role successfully removed');
+
     }
 }
